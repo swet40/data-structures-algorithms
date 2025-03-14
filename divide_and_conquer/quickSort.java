e6ce97feeaf2c2292ec7aed2ad437db6f7241602
Delta student <swetapal101112@gmail.com>
2025-03-14 19:50:46 +0530

    dc

--- divide_and_conquer/quickSort.java
+++ divide_and_conquer/quickSort.java
@@ -8,7 +8,34 @@ public class quickSort {
     }
 
     public static void QuickSort(int arr[], int si, int ei){
+        if(si>=ei){
+            return;
+        }
         
+        //last element- pivot
+        int pIdx = partition(arr, si, ei);
+        QuickSort(arr, si, pIdx-1); //left
+        QuickSort(arr, pIdx+1, ei); //right
+    }
+
+    public static int partition(int arr[], int si, int ei){
+        int pivot = arr[ei];
+        int i = si-1; //to make place for els smaller than pivot
+
+        for(int j = si; j<ei; j++){
+            if(arr[j] <= pivot){
+                i++;
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
     }
 
 
