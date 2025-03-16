e73c369f0ecc2ef5e0ec71c025431010b682b4d9
Sweta Pal <swetapal101112@gmail.com>
2025-03-16 20:14:25 +0530

    N-Queens firse krna hai yaar

---
+++ backtracking/subsets.java
@@ -0,0 +1,22 @@
+public class subsets {
+    public static void findSubsets(String str, String ans, int i){
+        //base case
+        if(i == str.length()){
+            if(ans.length() == 0){
+                System.out.println("null");
+            } else{
+                System.out.println(ans);
+            }
+            return;
+        }
+
+        //if chosen YES
+        findSubsets(str, ans+str.charAt(i), i+1);
+        //if chosen NO
+        findSubsets(str, ans, i+1);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubsets(str, "", 0);
+    }
+}
