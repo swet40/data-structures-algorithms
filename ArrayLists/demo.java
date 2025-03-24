769b90c63bc68c3d632d6cef17c57323c2b00f28
Sweta Pal <swetapal101112@gmail.com>
2025-03-24 06:55:22 +0530

    Arraylists

--- ArrayLists/demo.java
+++ ArrayLists/demo.java
@@ -1,19 +1,24 @@
 import java.util.*;
 
 public class demo {
+    public static void swap(ArrayList<Integer> list,int idx1, int idx2){
+        int temp = list.get(idx1);
+        list.set(idx1, list.get(idx2));
+        list.set(idx2, temp);
+    }
     public static void main(String[] args) {
         
         ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<String> list2 = new ArrayList<>();
-        ArrayList<Boolean> list3 = new ArrayList<>();
+        // ArrayList<String> list2 = new ArrayList<>();
+        // ArrayList<Boolean> list3 = new ArrayList<>();
 
-        list.add(1);  //O(1)
-        list.add(2);
-        list.add(3);
+        list.add(8);  //O(1)
+        list.add(4);
+        list.add(12);
         list.add(9);
         list.add(14);
 
-        System.out.println(list.size());
+        // System.out.println(list.size());
 
         //print the arraylist
         // for(int i = 0; i<list.size(); i++){
@@ -22,10 +27,10 @@ public class demo {
         // System.out.println();
 
         //Reverse print - O(n)
-        for(int i = list.size()-1; i>=0; i--){
-            System.out.print(list.get(i) + " ");
-        }
-        System.out.println();
+        // for(int i = list.size()-1; i>=0; i--){
+        //     System.out.print(list.get(i) + " ");
+        // }
+        // System.out.println();
 
         // list.add(1, 9);
 
@@ -46,5 +51,19 @@ public class demo {
         //contains
         // System.out.println(list.contains(1));
         // System.out.println(list.contains(10));
+
+        // int max = Integer.MIN_VALUE;
+        // for(int i = 0; i<list.size(); i++){
+        //     if(max < list.get(i)){
+        //         max = list.get(i);
+        //     }
+        // }
+        // System.out.println(max);
+        // int idx1 = 2, idx2 = 3;
+        // System.out.println(list);
+        // swap(list, idx1, idx2);
+        // System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
