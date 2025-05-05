55b669f948fe143b3204310225aee3bba40c50f0
Sweta Pal <swetapal101112@gmail.com>
2025-05-05 12:08:00 +0530

    k

---
+++ binary-trees/BST/balanced.java
@@ -0,0 +1,22 @@
+package BST;
+import java.util.*;
+
+
+public class balanced {
+    
+    public static class Node {
+        int data;
+        Node left,right;
+
+        public Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int createBST{
+        
+    }
+
+}
