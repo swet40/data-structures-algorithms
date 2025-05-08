77c12f6a502a6abf1b4c36717a1c7fff1b5283af
Sweta Pal <swetapal101112@gmail.com>
2025-05-08 11:15:42 +0530

    balanced tree

--- binary-trees/BST/balanced.java
+++ binary-trees/BST/balanced.java
@@ -15,8 +15,29 @@ public class balanced {
         }
     }
 
-    public static int createBST{
-        int mid = st+end/2;
+    public static void preOrder(Node root){
+        if(root == null) return;
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static Node createBST(int arr[], int st, int end){
+        if(st>end) return null;
+        int mid = (st+end)/2;
+
+        Node root = new Node(arr[mid]);
+        root.left = createBST(arr, st, mid-1);
+        root.right = createBST(arr, mid+1, end);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {3, 5, 6, 8, 10, 11, 12};
+
+        Node root = createBST(arr, 0, arr.length-1);
+        preOrder(root);
+
     }
 
 }
