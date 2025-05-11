733c227198e06cac87dcaf1bc22e737aade8fbb5
Sweta Pal <swetapal101112@gmail.com>
2025-05-11 20:57:49 +0530

    AVL started

---
+++ binary-trees/AVLtrees/basic.java
@@ -0,0 +1,20 @@
+public class basic{
+    public class Node{
+        int data;
+        Node left,right;
+
+        public Node{
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public class Node avl(Node root){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
