95fa6e5fea660af8d98f15312c51d70e29068414
Sweta Pal <swetapal101112@gmail.com>
2025-05-25 07:56:12 +0530

    nearby cars problem

--- Heaps/insert.java
+++ Heaps/insert.java
@@ -15,6 +15,9 @@ public class insert {
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
                 arr.set(par,temp);
+
+                x = par;
+                par = (x-1)/2;
             }
         }
 
@@ -38,7 +41,9 @@ public class insert {
             if(minIdx != i){
                 int temp = arr.get(i);
                 arr.set(i, arr.get(minIdx));
-                arr.set(arr.get(i), temp);
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
             }
         }
 
@@ -47,11 +52,30 @@ public class insert {
 
             //swap
             int temp = arr.get(0);
-            arr.set(0, arr.size()-1);
+            arr.set(0, arr.get(arr.size()-1));
             arr.set(arr.size()-1,temp);
+
+            arr.remove(arr.size()-1);
+
+            heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
         }
     }
+    
     public static void main(String[] args) {
-        
+        heap h = new heap();
+        h.add(3);
+        h.add(4);
+        h.add(5);
+        h.add(1);
+
+        while(!h.isEmpty()){
+            System.out.println(h.peek());
+            h.remove();
+        }
     }
 }
