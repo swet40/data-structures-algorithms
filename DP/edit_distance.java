826fc8c9fa9fc27dad05dfa0dba15c3441983c08
Sweta Pal <swetapal101112@gmail.com>
2025-10-02 16:31:15 +0530

    edit distance

---
+++ DP/edit_distance.java
@@ -0,0 +1,39 @@
+public class edit_distance{
+    // O(n*m)
+    public static int editdis(String s1, String s2){
+        int n = s1.length();
+        int m = s2.length();
+
+        int dp[][] = new int[n+1][m+1];
+
+        //initialization
+        for(int i=0; i<n+1; i++){
+            for(int j = 0; j<m+1; j++){
+                if(i==0) dp[i][j] = j;
+                if(j==0) dp[i][j] = i;
+            }
+        }
+
+        //bottom-up
+        for(int i = 1; i<n+1; i++){
+            for(int j=1; j<m+1; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){  //same
+                    dp[i][j] = dp[i-1][j-1];
+                } else{  //diff
+                    int add = dp[i][j-1]+1;
+                    int del = dp[i-1][j]+1;
+                    int rep = dp[i-1][j-1]+1;
+                    dp[i][j] = Math.min(add, Math.min(del, rep));
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+    public static void main(String[] args) {
+        String word1 = "intention";
+        String word2 = "execution";
+
+        System.out.println(editdis(word1, word2));
+    }
+}
