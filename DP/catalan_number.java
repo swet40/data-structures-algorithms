738415cd1d86d5402964100c9b4698da3bb72fb8
Sweta Pal <swetapal101112@gmail.com>
2025-10-10 17:52:46 +0530

    count bsts

--- DP/catalan_number.java
+++ DP/catalan_number.java
@@ -4,6 +4,7 @@ public class catalan_number{
         dp[0] = 1;
         dp[1] = 1;
 
+        // O(n*n)
         for(int i = 2; i<=n; i++){
             for(int j=0; j<i; j++){
                 dp[i] += dp[j]*dp[i-j-1];
@@ -12,7 +13,7 @@ public class catalan_number{
         return dp[n];
     }
     public static void main(String[] args) {
-        int n = 3;
+        int n = 4;
         System.out.println(catalan(n));
     }
 }
